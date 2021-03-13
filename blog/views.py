db3c0df27a57c15390b3a10f768c57ebe22ef625
LordRonz <aarontanhar2000@gmail.com>
2021-03-13 21:29:47 +0700

    compress profile image

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -8,5 +9,17 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
